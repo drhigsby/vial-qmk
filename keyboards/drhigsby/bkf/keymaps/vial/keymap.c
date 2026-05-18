f0c926056c113fa498431c1784db361c0440b6f4
drhigsby <doctorhigsby@gmail.com>
2026-05-18 12:10:02 -0700

    BKF: update vial keymap and layout

--- keyboards/drhigsby/bkf/keymaps/vial/keymap.c
+++ keyboards/drhigsby/bkf/keymaps/vial/keymap.c
@@ -33,16 +33,16 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_2x3uC(
-        QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,
+        QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,          KC_ENT,
-        SC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          SC_RSPC,
-        KC_LCTL, KC_LGUI, KC_LALT,          NUM,              KC_SPC,           FN,      _______, KC_QUOT
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+        KC_LALT, KC_LGUI, KC_LCTL,          NUM,              KC_SPC,           FN,      KC_RGUI, _______
     ),
 
     [_NUM] = LAYOUT_2x3uC(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC,
         KC_CAPS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,           KC_PIPE,
-        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END,  _______, _______, _______, _______,          _______,
+        _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_HOME, KC_END,  _______, _______, _______, _______,          _______,
         _______, _______, _______,          _______,          _______,          _______, _______, _______
     ),
 
