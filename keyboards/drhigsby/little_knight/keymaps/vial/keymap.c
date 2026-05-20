0568ef461f8af8de8de2d50156b86013be96bfe8
drhigsby <doctorhigsby@gmail.com>
2026-05-20 07:51:33 -0700

    update vial and default keymaps

--- keyboards/drhigsby/little_knight/keymaps/vial/keymap.c
+++ keyboards/drhigsby/little_knight/keymaps/vial/keymap.c
@@ -16,41 +16,38 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _BASE,
-    _LOWER,
-    _RAISE,
-    _ADJUST
+    _0,
+    _1,
+    _2,
+    _3
 };
-
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_BASE] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_LCTL, LOWER,   KC_SPC,  KC_MUTE, KC_SPC,  RAISE,   KC_RSFT
+    [_0] = LAYOUT(
+        KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        LSFT_T(KC_Z),   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
+        KC_LALT,        KC_LCTL, MO(_1),  KC_DEL,  KC_SPC,  MO(_2),  MO(_3)
     ),
 
-    [_LOWER] = LAYOUT(
+    [_1] = LAYOUT(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_TAB,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_MUTE, _______, _______, _______
+    ),
+
+    [_2] = LAYOUT(
+        _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______
     ),
 
-    [_RAISE] = LAYOUT(
+    [_3] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-        KC_ESC,  _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______
-    ),
-
-    [_ADJUST] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_F11,  KC_F12,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______
     )
@@ -58,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_BASE]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_LOWER]  = { ENCODER_CCW_CW(KC_WH_D, KC_WH_U) },
-    [_RAISE]  = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
-    [_ADJUST] = { ENCODER_CCW_CW(_______, _______) }
+    [_0] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
+    [_1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_2] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
+    [_3] = { ENCODER_CCW_CW(_______, _______) }
 };
 #endif
